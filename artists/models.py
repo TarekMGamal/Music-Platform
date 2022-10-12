e7252b647418bdcafcd0794126864ffaf431d24b
Tarek Gamal <TarekMGamal1@gmail.com>
2022-10-12 14:34:27 +0200

    feat: Add Artist model in artists app

--- artists/models.py
+++ artists/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Artist(models.Model):
+    stage_name = models.CharField(max_length=100, unique=True, blank=False)
+    social_media_link = models.URLField(max_length=500, blank=True, null=False)
+    
+    class Meta:
+        ordering = ['stage_name']
+
+    def __str__(self):
+        return self.stage_name
+    
--- music_platform/settings.py
+++ music_platform/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'artists.apps.ArtistsConfig',
+    'albums.apps.AlbumsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
