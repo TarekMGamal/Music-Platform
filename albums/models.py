79a86220bb07630d9e8d18d957408eb26dcc0bfd
Tarek Gamal <TarekMGamal1@gmail.com>
2022-10-12 15:15:19 +0200

    feat: Add Album model in albums app

--- albums/models.py
+++ albums/models.py
@@ -1,3 +1,11 @@
+from enum import auto
+from unicodedata import decimal
 from django.db import models
+from artists.models import Artist
 
-# Create your models here.
+class Album(models.Model):
+    name = models.CharField(max_length=100, default='New Album')
+    creation_datetime = models.DateTimeField(auto_now_add=True)
+    release_datetime = models.DateTimeField(blank=False)
+    cost = models.DecimalField(blank=False, max_digits=9, decimal_places=2)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
