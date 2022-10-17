551fabee6541f0d4ccf724c9ed0393be3d9bf4c2
Tarek Gamal <TarekMGamal1@gmail.com>
2022-10-17 12:26:47 +0200

    feat: Make creation_datetime field readonly

--- albums/admin.py
+++ albums/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Album
 
-admin.site.register(Album)
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    readonly_fields = ('creation_datetime',)
