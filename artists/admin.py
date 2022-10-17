869c80963d5dce031b1bdc274a21384e3eace559
Tarek Gamal <TarekMGamal1@gmail.com>
2022-10-17 16:22:20 +0200

    feat: Add approved albums number column to artist

--- artists/admin.py
+++ artists/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
+
+from albums.models import Album
 from .models import Artist
 
-admin.site.register(Artist)
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('stage_name', 'get_approved_albums_number')
+    
+    def get_approved_albums_number(self, artist_obj):
+        approved_albums_number = Album.objects.filter(artist = artist_obj, isApproved = True).count()
+        return approved_albums_number
+        
