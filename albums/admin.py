8e6ca1ec62ae30dce94f013799e9498a97c50953
Tarek Gamal <TarekMGamal1@gmail.com>
2022-10-17 15:33:49 +0200

    feat: Add description to isApproved field

--- albums/admin.py
+++ albums/admin.py
@@ -4,3 +4,16 @@ from .models import Album
 @admin.register(Album)
 class AlbumAdmin(admin.ModelAdmin):
     readonly_fields = ('creation_datetime',)
+    
+    fieldsets = (
+        (None, { "fields": ('name',) }),
+        (None, { "fields": ('artist',) }),
+        (None, { "fields": ('release_datetime',) }),
+        (None, { "fields": ('creation_datetime',) }),
+        (None, { "fields": ('cost',) }),
+        (None, {
+            "fields": ('isApproved',),
+            "description": 'Approve the album if its name is not explicit'
+        })
+    )
+    
