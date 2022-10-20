635991d3257ecccdf0a022bad0c74feee3a16e11
Tarek Gamal <TarekMGamal1@gmail.com>
2022-10-20 17:14:52 +0200

    feat: Add artists view

--- artists/views.py
+++ artists/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from albums.models import Album
 
-# Create your views here.
+def artists(request):
+    albums = Album.objects.select_related('artist').order_by('artist').all()
+    data = {
+        'albums': albums
+    }
+    return render(request , 'artists/artists.html', data)
