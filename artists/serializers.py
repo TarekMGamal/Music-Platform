d6dc6477d3f3acb74717173f53cdc4a85f58cf7d
Tarek Gamal <TarekMGamal1@gmail.com>
2022-10-27 13:54:11 +0200

    refactor: Update artists view to be REST API view

---
+++ artists/serializers.py
@@ -0,0 +1,8 @@
+from dataclasses import fields
+from rest_framework import serializers
+from .models import Artist
+
+class artist_serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Artist
+        fields = ['id', 'stage_name', 'social_media_link']
