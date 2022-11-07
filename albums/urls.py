c69af8e9d19117f9e85d6e319c9de0aa9cab19eb
Tarek Gamal <TarekMGamal1@gmail.com>
2022-11-07 14:38:22 +0200

    feat: Add albums manual filter view

--- albums/urls.py
+++ albums/urls.py
@@ -5,5 +5,6 @@ from django_filters.views import FilterView
 urlpatterns = [
     path('create/', views.albums_create.as_view(), name="albums-create"),
     path('', views.albums.as_view(), name="albums"),
-    path('filter/', FilterView.as_view(filterset_class=views.albums_filter))
+    path('filter/', FilterView.as_view(filterset_class=views.albums_filter)),
+    path('filter/manual/', views.albums_manual_filter.as_view()),
 ]
--- albums/views.py
+++ albums/views.py
@@ -3,7 +3,7 @@ from .forms import CreateAlbumForm
 from django.views.generic import CreateView
 from .models import Album
 from django.contrib.auth.mixins import LoginRequiredMixin
-from rest_framework import generics, pagination, permissions
+from rest_framework import generics, pagination, permissions, response
 from .serializers import album_serializer
 from django_filters import rest_framework as filters
 
@@ -32,3 +32,28 @@ class albums(generics.ListCreateAPIView):
     
     def perform_create(self, serializer):
         serializer.save(artist=self.request.user.artist)
+        
+        
+class albums_manual_filter(generics.ListCreateAPIView):
+    queryset = Album.objects.filter(is_approved=True)
+    pagination_class = pagination.LimitOffsetPagination
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    serializer_class = album_serializer
+    
+    def perform_create(self, serializer):
+        serializer.save(artist=self.request.user.artist)
+        
+    def list(self, request):
+        cost_greater_than_equal = request.GET.get('cost__gte', 0)
+        cost_less_than_equal = request.GET.get('cost__lte', 999999999.99)
+        name_contains = request.GET.get('name__icontains', '')
+        
+        queryset = self.get_queryset()
+        queryset = queryset.filter(cost__gte=cost_greater_than_equal, 
+                                   cost__lte=cost_less_than_equal, 
+                                   name__icontains=name_contains)
+        
+        serializer = album_serializer(queryset, many=True)
+        data = serializer.data
+        
+        return response.Response(data)
